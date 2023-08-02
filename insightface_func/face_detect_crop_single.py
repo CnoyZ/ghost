d068328f06045e3035807d4b2235416204e9fbfd
CnoyZ <117410853+CnoyZ@users.noreply.github.com>
2023-08-02 13:32:52 +0100

    Update face_detect_crop_single.py
    correct the output of face_align.estimate_norm

--- insightface_func/face_detect_crop_single.py
+++ insightface_func/face_detect_crop_single.py
@@ -81,7 +81,7 @@ class Face_detect_crop:
         kps = None
         if kpss is not None:
             kps = kpss[best_index]
-        M, _ = face_align.estimate_norm(kps, crop_size, mode ='None') 
+        M = face_align.estimate_norm(kps, crop_size, mode ='None') 
         align_img = cv2.warpAffine(img, M, (crop_size, crop_size), borderValue=0.0)
         
         return [align_img], [M]
